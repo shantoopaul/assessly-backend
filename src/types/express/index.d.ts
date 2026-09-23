2c2dd27bc9525eb49d4d3c18a506ad14382a0c6d
shantoopaul <shantoopaul@gmail.com>
2026-09-23 19:16:00 +0600

    change: express type

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,12 +1,14 @@
 import type { Role } from "../../../generated/prisma/enums";
 
-declare module "express-serve-static-core" {
-	interface Request {
-		user?: {
-			id: string;
-			email: string;
-			role: Role;
-		};
+declare global {
+	namespace Express {
+		interface Request {
+			user?: {
+				id: string;
+				email: string;
+				role: Role;
+			};
+		}
 	}
 }
 
